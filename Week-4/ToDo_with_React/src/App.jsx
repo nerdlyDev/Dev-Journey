2eea52dd1597ba0688ecb00c5d7d26d2e449d979
Devesh-code <developersharma1999@gmail.com>
2024-03-29 01:48:09 +0530

    getting stated with React

---
+++ Week-4/ToDo_with_React/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import './App.css'
+
+// Dynamic websites
+// * When u have to create a dynamic website, you write a lot of js code that does DOM manipulation
+
+// * What is react?
+// * Is it website creation framework?
+// * Or is react-dom the website creation framework ?
+// * If it is rect-dom, then what is react? What is the need of react ? 
+
+function App() {
+  const [count, setCount] = useState(0)
+  return (
+      <div>
+
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+      </div>
+  )
+}
+
+export default App
