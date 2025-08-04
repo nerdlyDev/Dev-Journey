f5bfda65266e5f057c09cabdb6779a47ad5e1b8f
Devesh-code <developersharma1999@gmail.com>
2025-08-05 00:33:53 +0530

    learning postgress sql and docker

--- Week-10/src/utils.ts
+++ Week-10/src/utils.ts
@@ -1,7 +1,9 @@
-import { Client } from 'pg';
+import { Client } from "pg";
 
 export async function getClient() {
-    const client = new Client("postgres://wzsxsnxg:LHZ9Cv4QoZ1zctxapkOq2ch672-o9UQe@trumpet.db.elephantsql.com/wzsxsnxg");
-    await client.connect();
-    return client;
+  const client = new Client(
+    "postgres://wzsxsnxg:LHZ9Cv4QoZ1zctxapkOq2ch672-o9UQe@trumpet.db.elephantsql.com/wzsxsnxg"
+  );
+  await client.connect();
+  return client;
 }
